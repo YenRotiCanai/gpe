e7a48f043c7f56f29b4dbbf49e20a59e333cd894
learninglab <learninglab@LLM.local>
2020-09-14 11:02:08 +0800

    10402Doe

---
+++ p10402_WhatsCyptanalysis.java
@@ -0,0 +1,31 @@
+//http://hellowalk.blogspot.com/2018/07/java-uva-10008-whats-cryptanalysis.html
+import java.util.*;
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.valueOf(sc.nextLine());
+		int arr[] = new int[200];
+		int len = 0;
+
+		while(n-->0){
+			String str = sc.nextLine();
+			len += str.length();
+			// System.out.println(len);
+			str = str.toUpperCase();
+			for(int i=0; i<str.length(); i++){
+				if(str.charAt(i) >= 'A' && str.charAt(i) <='Z'){
+					arr[(int)str.charAt(i)]++;
+				}
+			}
+		}
+
+		while(len-->1){
+			for(int i='A'; i<='Z'; i++){
+				if(arr[i] == len) System.out.println((char)i + " "+ len);
+			}
+		}
+
+		// System.out.println(Arrays.toString(arr));
+	}
+}
