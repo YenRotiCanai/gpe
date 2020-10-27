44aaa9411852a457a6828405d7404543c96cfbab
YenRotiCanai <lnight94@gmail.com>
2020-10-27 21:54:39 +0800

    rev

---
+++ book/2star/p10552_AutomatedJudgeScript.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class p10552_AutomatedJudgeScript {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int run = 1; //記錄執行次數
+
+        while(sc.hasNext()){
+            //讀入正確答案的行數
+            int correctLineCount = sc.nextInt();
+            if(correctLineCount==0) break;
+
+            sc.nextLine(); //吃掉換行
+            String correct = ""; //宣告一個String來放正確答案
+            //把答案都先串起來
+            for(int i=0; i<correctLineCount; i++){
+                correct += sc.nextLine() + "\n";
+            }
+            System.out.println(correct);
+
+            //接下來是提交的答案
+            //一樣先讀取行數
+            int submittedLineCount = sc.nextInt();
+            sc.nextLine();//吃掉換行
+            String submitted = ""; //宣告一個String來放，然後再把他串起來
+            for(int i=0; i<submittedLineCount; i++){
+                submitted += sc.nextLine() + "\n";
+            }
+
+            //到判斷步驟，把非字元拿掉，留下數字
+            String correctNumbers = correct.replaceAll("[^0-9]+", "");
+            String submittedNumbers = submitted.replaceAll("[^0-9]+", "");
+
+            System.out.print("Run #"+run+": ");
+            if(correct.equals(submitted))System.out.println("Accepted");
+            else if(correctNumbers.equals(submittedNumbers)) System.out.println("Presentation Error");
+            else System.out.println("Wrong Answer");
+
+            run++;
+        }
+    }
+}
--- book/rev.java
+++ book/rev.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
-import java.util.HashMap;
 
 class rev{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int run = 1;
+        
         while(sc.hasNext()){
-            int m = sc.nextInt();
-            int n = sc.nextInt();
-            HashMap<String, Integer> hm = new HashMap<String, Integer>();
+            int correctLineCounts = sc.nextInt();
+            if(correctLineCounts == 0) break;
 
-            for(int i=0; i<m; i++){
-                hm.put(sc.next(), sc.nextInt());
+            sc.nextLine();
+            String correct = "";
+            for(int i=0; i<correctLineCounts; i++){
+                correct += sc.nextLine()+"\n";
             }
 
-            for(int j=0; j<n; j++){
-                String str;
-                int sum = 0;
-                while(!(str = sc.next()).equals(".")){
-                    if(hm.containsKey(str)) sum += hm.get(str);
-                }
-                System.out.println(sum);
+            int submittedLineCounts = sc.nextInt();
+            sc.nextLine();
+            String submitted = "";
+            for(int i=0; i<submittedLineCounts; i++){
+                submitted += sc.nextLine()+"\n";
             }
+
+            String correctNumbers = correct.replaceAll("[^0-9]+","");
+            String submittedNumbers = submitted.replaceAll("[^0-9]+","");
+            
+            System.out.print("Run #"+run+": ");
+            if(correct.equals(submitted)) System.out.println("Accepted");
+            else if(correctNumbers.equals(submittedNumbers)) System.out.println("Presentation Error");
+            else System.out.println("Wrong Answer");
+
+            run ++;
         }
         sc.close();
     }
