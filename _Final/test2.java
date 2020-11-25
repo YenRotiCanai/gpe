af8ebae1054c97d632dbf6be342921761bbaeb7a
YenRotiCanai <lnight94@gmail.com>
2020-11-25 17:35:14 +0800

    +uu

---
+++ _Final/test2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+  
+  class p10402_WhatsCryptanalysis {
+      public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+          
+          char c;
+          int len = 0;
+          int arr[] = new int[300];
+          String s;
+  
+          int n = Integer.parseInt(sc.nextLine());
+  
+          while(n-->0){
+              s = sc.nextLine();
+  
+              for(int i=0; i<s.length(); i++){
+                  c = s.toUpperCase().charAt(i);
+                  arr[c]++;
+                  len++;
+              }
+          }
+  
+          while(--len>0){
+              for(c='A'; c<='Z'; c++){
+                  if(len == arr[c]) System.out.println(c+" "+len);
+              }
+          }
+          sc.close();
+     }
+}
--- temp.java
+++ temp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class temp {
-    public static void main(String[] args) throws Throwable{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
     }
