04de900c80c06fe9f9f224793b8c983664cd49b5
YenRotiCanai <lnight94@gmail.com>
2021-01-16 06:29:43 +0800

    morning

--- _FFinal/p21914_RotatingSentences.cpp
+++ _FFinal/p21914_RotatingSentences.cpp
@@ -21,9 +21,10 @@ int main(){
 		
 		if(len[n]>max) max = len[n]; //調整最大長度
 
+		//如果之後的長度沒有到最大長度，就補空格
 		for(int add = len[n]; add < max; add++){
 			str[n][add] = ' ';
-			len[n]++;
+			len[n]++; //一遍補一遍增加該行的長度，讓他最後跟最大長度一樣
 		}
 		n++;
 
@@ -34,8 +35,9 @@ int main(){
 	//第二圈是有多少行，從最後一行開始
 	for(int j=0; j<max; j++){
 		for(int i=n-1; i>=0; i--){
-			//
-			if(j<len[i]) cout << str[i][j];
+			//因為長度是越來越長，所以一開始的後面如果是比較短的話，就會有很多空白
+			//用這可以判斷後面印到一開始的資料時，那些空白不用印出
+			if(j<len[i]) cout << str[i][j]; 
 		}
 		cout << endl;
 	}
