69d2abcec7438c2eb4fdc368cb8a98eefbb127b7
YenMilo <yenann.cs06g@nctu.edu.tw>
2021-01-17 23:33:32 +0800

    robot

--- _FFinal/p24911_RobotInstructions.java
+++ _FFinal/p24911_RobotInstructions.java
@@ -1,3 +1,6 @@
+//不能直接用 Integer.parseInt(sc.nextLine()) 來吃測資，會 TLE
+//只好吃 int，然後吃完後要吃換行
+
 import java.util.*;
 
 class robot{
@@ -11,6 +14,9 @@ class robot{
 				int n = sc.nextInt();
 				sc.nextLine();
 
+				//開一個陣列把指令存起來，如果是left就存 -1，如果是right就存 1
+				//最後把陣列裡的全部加起來就是答案了
+				//如果遇到same as的，就用substring(8)，來取得跟他同樣指令的樓層，然後丟給目前的層數
 				int arr[] = new int[n+1];
 
 				for(int i=1; i<=n; i++){
