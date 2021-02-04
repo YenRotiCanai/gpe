defbaf15468d9795949425a149520036b11bcaa6
YenRotiCanai <lnight94@gmail.com>
2021-02-04 18:50:54 +0800

    2/4

--- 3R/c1.cpp
+++ 3R/c1.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 int main(){
-    long n;
-	while(cin >> n && n!=0){
-        long m = (long)sqrt(n);
-        cout << (m*m==n? "yes":"no") << endl;
+    int cases;
+    cin >> cases;
+
+    while(cases--){
+        int n, sum=0;
+        string str, str2;
+
+        cin >> n;
+        int arr[n+1];
+        cin.ignore();
+
+        for(int i=1; i<=n; i++){
+            getline(cin, str);
+
+            if(str == "LEFT") arr[i] = -1;
+            else if(str == "RIGHT") arr[i] = 1;
+            else{
+                string str2 = str.substr(8);
+                istringstream isst(str2);
+                int tmp;
+                isst >> tmp;
+                //int tmp = stoi(str2); stoi 要 c++11 才可以用，不然只能用istringstream
+                arr[i] = arr[tmp];
+            }
+            sum += arr[i];
+        }
+        cout << sum << endl;
     }
 }
