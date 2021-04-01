0ccf38cebf8509a7bbd8a7aee6d015857cf8bc96
YenRotiCanai <lnight94@gmail.com>
2021-04-01 18:54:17 +0800

    +

--- 3R/10461_Parity.cpp
+++ 3R/10461_Parity.cpp
@@ -6,17 +6,17 @@ int main(){
     stack<int> stk;
 
     while(cin>>n && n){
-        int parity = 0;
+        int parity = 0; //要加起來的
         while(n){
             parity += n%2;
-            stk.push(n%2);
+            stk.push(n%2); //把結果算好後，推進stack
             n/=2;
         }
 
         cout << "The parity of ";
         while(stk.size()){
-            cout << stk.top();
-            stk.pop();
+            cout << stk.top(); //從 stack 的上面開始叫
+            stk.pop(); //每叫完一個就丟掉一個
         }
         cout << " is "<< parity << " (mod 2)." << endl;
     }
