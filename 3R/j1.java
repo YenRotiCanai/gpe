dc06e5aa0f37617ec10a18c59ceccdf58a5fb5bf
YenRotiCanai <lnight94@gmail.com>
2021-04-10 21:09:49 +0800

    a

--- 3R/j1.java
+++ 3R/j1.java
@@ -1,36 +1,41 @@
-//https://a7069810.pixnet.net/blog/post/394903351-%5Buva10062%5D-tell-me-the-frequencies%21
 import java.util.*;
 
 class Main{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		boolean newLine = false;
 
-		while(sc.hasNext()){
-			if(newLine) System.out.println();
+		int n = sc.nextInt();
 
-			String str = sc.nextLine();
-			int arr[] = new int[128]; //雖然範圍是從32-128，但是不用管那麼多，直接用 128
+		while(n-->0){
+			int len = sc.nextInt();
 
-			//統計每個字元出現的次數
-			for(int i=0; i<str.length(); i++){
-				arr[str.charAt(i)]++;
+			int arr[] = new int[len];
+
+			for(int i=0; i<len; i++){
+				arr[i] = sc.nextInt();
 			}
+			//System.out.println(Arrays.toString(arr));
 
-			//找出最多次數的那一個
-			int max = 0;
-			for(int i=0; i<arr.length; i++){
-				if(arr[i]>max) max = arr[i];
-			}
+			int count = 0;
 
-			//由最少次數，從陣列的最後面開始找，這樣就符合題目要求的先從小到大印出次數，再來才是看ASCII的順序
-			for(int i=1; i<=max; i++){
-				for(int j=arr.length-1; j>=0; j--){
-					if(i == arr[j]) System.out.println(j+" "+i);
+			boolean sorted = true;
+			
+			int pt = 0;
+			while(sorted){
+				sorted = false;
+				for(int i=0; i<len-1; i++){
+					if(arr[i]>arr[i+1]){
+						int tmp = arr[i];
+						arr[i] = arr[i+1];
+						arr[i+1] = tmp;
+						sorted = true;
+						count++;
+					}
 				}
+				
 			}
-			newLine = true;
+
+			System.out.printf("Optimal train swapping takes %d swaps.%n",count);
 		}
-		
 	}
 }
