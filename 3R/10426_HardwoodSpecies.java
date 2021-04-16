5a8141eb29476c4893ef6528ecaace467d5d1647
YenRotiCanai <lnight94@gmail.com>
2021-04-16 18:37:20 +0800

    a

--- 3R/10426_HardwoodSpecies.java
+++ 3R/10426_HardwoodSpecies.java
@@ -5,14 +5,14 @@ class Main{
 		Scanner sc = new Scanner(System.in);
 
 		int n = Integer.parseInt(sc.nextLine());
-		sc.nextLine();
-
-		boolean first = true;
 		
+		sc.nextLine();
+		boolean first=true;
+
 		while(n-->0){
 			if(!first) System.out.println();
 
-			HashMap<String,Integer> hm = new HashMap<String, Integer>();
+			HashMap<String,Integer> hm = new HashMap<String,Integer>();
 			ArrayList<String> arrlist = new ArrayList<String>();
 
 			float total = 0.0f;
@@ -22,10 +22,12 @@ class Main{
 
 				if(str.length() == 0) break;
 
+				//檢查看裡面是不是已經有 key 了，如果有的話，就把它的撈出來 +1
+				//沒有的話，就把它加進去
 				if(hm.containsKey(str)){
 					hm.put(str, hm.get(str)+1);
 				}else{
-					hm.put(str, 1);
+					hm.put(str,1);
 					arrlist.add(str);
 				}
 
@@ -35,6 +37,8 @@ class Main{
 			Collections.sort(arrlist);
 
 			for(String s:arrlist) System.out.printf("%s %.4f%n", s, (float)hm.get(s)/total*100);
+
+			first = false;
 		}
 	}
 }
--- 3R/j1.java
+++ 3R/j1.java
@@ -4,38 +4,52 @@ class Main{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 
-		int n = Integer.parseInt(sc.nextLine());
-		sc.nextLine();
-		boolean first=true;
+		int field = 1;
+		while(sc.hasNext()){
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			if(h==0 && w==0) break;
 
-		while(n-->0){
-			if(!first) System.out.println();
+			//題目要求從第一題開始後面要空一行
+			if(field!=1) System.out.println();
 
-			HashMap<String,Integer> hm = new HashMap<String,Integer>();
-			ArrayList<String> arrlist = new ArrayList<String>();
-
-			float total = 0.0f;
-
-			while(sc.hasNext()){
-				String str = sc.nextLine();
-
-				if(str.length() == 0) break;
-
-				//檢查看裡面是不是已經有 key 了，如果有的話，就把它的撈出來 +1
-				//沒有的話，就把它加進去
-				if(hm.containsKey(str)){
-					hm.put(str, hm.get(str)+1);
-				}else{
-					hm.put(str,1);
-					arrlist.add(str);
+			//統計地雷的地圖，要先初始化為 0
+			char map[][] = new char[h+5][w+5];
+			for(int i=1; i<=h; i++){
+				for(int j=1; j<=w; j++){
+					map[i][j] = '0';
 				}
-
-				total++;
 			}
 
-			Collections.sort(arrlist);
+			//讀入地圖
+			for(int i=1; i<=h; i++){
+				String str = sc.next();
+				for(int j=1; j<=w; j++){
+					char d = str.charAt(j-1);
 
-			for(String s:arrlist) System.out.printf("%s %.4f%n", s, (float)hm.get(s)/total*100);
+					//如果是地雷，先看周圍是不是地雷，不是的話
+                    //就把他們都+1
+					if(d=='*'){
+						map[i][j] = '*';
+
+						for(int m=-1; m<=1; m++){
+							for(int n=-1; n<=1; n++){
+								if(map[m+i][n+j]!='*')
+									map[m+i][n+j]++;
+							}
+						}
+					}
+				}
+			}
+
+			System.out.println("Field #"+field+":");
+			for(int i=1; i<=h; i++){
+				for(int j=1; j<=w; j++){
+					System.out.print(map[i][j]);
+				}
+				System.out.println();
+			}
+			field++;
 		}
 	}
 }
