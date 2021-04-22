1d812738727a7c505c2243572c9e12fb510a5692
YenRoti <lnight94@gmail.com>
2021-04-22 06:57:15 +0000

    a

---
+++ 3R/10582_PowerStrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			String str = sc.nextLine();
+			if(str.equals(".")) break;
+
+			char cArr[] = str.toCharArray();
+			int len = str.length();
+			boolean flag = false;
+
+			for(int i=1; i<=len; i++){
+				if(len%i != 0) continue;
+
+				flag = true;
+				for(int j=i; j<len && flag==true; j+=i){
+					for(int k=0; k<i && flag==true; k++){
+						if(cArr[k] != cArr[k+j]) flag = false;
+					}
+				}
+				if(flag){
+					System.out.println(len / i);
+					break;
+				}
+			}
+		}
+		
+	}
+}
