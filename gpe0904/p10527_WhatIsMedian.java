27e333177a2ebd9fa82f59841c14e127a4e518bf
YenMilo <yenann.cs06g@nctu.edu.tw>
2021-04-26 17:25:27 +0800

    拜託讓我過

--- gpe0904/p10527_WhatIsMedian.java
+++ gpe0904/p10527_WhatIsMedian.java
@@ -7,23 +7,25 @@ class p10527 {
         int count = 1;
 
         while(sc.hasNext()){
-            arr[count] = sc.nextInt(); //把每一個 N 都塞進去 arr 裡面
+            //把每一個 N 都塞進去 arr 裡面
+            arr[count] = sc.nextInt(); 
 
             //開始插值
             for(int i = 1; i < count; i++){
 
-                //System.out.println("count:"+count+", arrCount:"+arr[count]+", i:"+i+" arr[i]:"+arr[i]);
-
-                if(arr[count] <= arr[i]){       //將arr裡面的一個個做比較,如果發現他比裡面其中一個小的話，就要換位子
-                    int tmp = arr[i];           //先把原本 arr 裡面的東西給 tmp
-                    arr[i] = arr[count];        //然後把他丟到他應該在的位置，這時候的i已經放新的值進去了
-                    //System.out.println("arr:"+Arrays.toString(arr)+", tmp:"+tmp);
+                //將arr裡面的一個個做比較,如果發現他比裡面其中一個小的話，就要換位子
+                if(arr[count] <= arr[i]){       
+                    //先把原本 arr 裡面的東西給 tmp
+                    int tmp = arr[i];
+                    //然後把他丟到他應該在的位置，這時候的i已經放新的值進去了
+                    arr[i] = arr[count];        
+                    System.out.println("arr:"+Arrays.toString(arr)+", tmp:"+tmp);
 
                     //這裡就是把 i 後面的往後挪一個
                     for(int j = i; j < count; j++){
                         int tmp2 = arr[j+1];    //先把後面原本的放到 tmp2
                         arr[j+1] = tmp;         //然後把剛剛被換掉的插進去
-                        //System.out.println("arr:"+Arrays.toString(arr)+", tmp:"+tmp+", tmp2:"+tmp2);
+                        System.out.println("arr:"+Arrays.toString(arr)+", tmp:"+tmp+", tmp2:"+tmp2);
                         tmp = tmp2;             //接著再把 tmp2 丟回給 tmp,等下一次的插入就會插進去了
                         
                     }
