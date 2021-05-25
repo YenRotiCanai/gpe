e51ad3f5ad46e2e68f5251e08fd527b9717b886d
YenRotiCanai <lnight94@gmail.com>
2021-05-25 23:44:09 +0800

    +u

--- cpe/1star/c1.cpp
+++ cpe/1star/c1.cpp
@@ -1,27 +1,10 @@
+#include <iostream>
 #include <cstdio>
+using namespace std;
+
 
 int main(){
-    int n;
-    double x1, y1, x2, y2;
-
-    while(scanf("%d %lf %lf %lf %lf", &n, &x1, &y1, &x2, &y2) == 5){
-        bool escape = false;
-        double x, y, ansx, ansy;
-
-        while(n--){
-            scanf("%lf %lf", &x, &y);
-            if(escape) continue;
-
-            if(4*((x1-x)*(x1-x) + (y1-y)*(y1-y)) <= (x2-x)*(x2-x) + (y2-y)*(y2-y)){
-                escape = true;
-                ansx = x;
-                ansy = y;
-            }
-        }
-
-        if(escape) printf("The gopher can escape through the hole at (%.3lf,%.3lf).\n", ansx, ansy);
-        else puts("The gopher cannot escape.");
-    }
+    unsigned long arr[]    
 
     return 0;
 }
--- cpe/1star/c2.cpp
+++ cpe/1star/c2.cpp
@@ -1,24 +1,26 @@
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
- 
-using namespace std;
- 
-int main()
-{
-	char   buf[128];
-	int    c,h,m,s,t0 = 0,t1 = 0;
-	double d = 0.0,v0 = 0.0,v1 = 0.0;
-	while ( gets(buf) ) {
-		c  = sscanf(buf,"%d:%d:%d %lf",&h,&m,&s,&v1);
-		t1 = h*3600+m*60+s;
-		d += v0*(t1-t0)/3600.0;
-		t0 = t1;
-		if ( c == 3 )
-			printf("%.2d:%.2d:%.2d %.2lf km\n",h,m,s,d);
-		else v0 = v1;
-	}
-	
-	return 0;
+#include <stdio.h>
 
+int main() {
+    int n;
+    double x1,y1, x2, y2;
+    while(scanf("%d %lf %lf %lf %lf", &n, &x1, &y1, &x2, &y2) == 5) {
+        int find = 0;
+        double x, y, ansx, ansy;
+        while(n--) {
+            scanf("%lf %lf", &x, &y);
+            if(find)
+                continue;
+            if(4*((x1-x)*(x1-x)+(y1-y)*(y1-y)) <= (x2-x)*(x2-x)+(y2-y)*(y2-y)) {
+                find = 1;
+                ansx = x;
+                ansy = y;
+            }
+        }
+        if(find) {
+            printf("The gopher can escape through the hole at (%.3lf,%.3lf).\n", ansx, ansy);
+        } else {
+            puts("The gopher cannot escape.");
+        }
+    }
+    return 0;
 }
