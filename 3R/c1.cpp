dd15b70212e49996803feabbf33a6d00d2998b65
YenRotiCanai <lnight94@gmail.com>
2021-06-01 07:04:03 +0800

    gogogo

--- 3R/c1.cpp
+++ 3R/c1.cpp
@@ -3,18 +3,27 @@
 #include <cstdlib>
 using namespace std;
 
-
 int main(){
-    int n, k, newK, total;
+    int M = 9;
+    int b = M-1;
 
-    while(scanf("%d %d", &n, &k) != EOF){
-        newK = 0, total = n;
+    int topb = b;
+    while(topb--) cout <<" ";
+    cout <<"/\\"<<endl;
 
-        while(n >= k){
-            newK = n/k;
-            total += newK;
-            n = n % k + newK;
-        }
-        printf("%d\n", total);
+    for(int i=2; i<=M-1; i++){
+        b--;
+        int medb = b;
+        while(medb--) cout <<" ";
+
+        int spaces = 2*(i-1);
+        cout << "/";
+        while(spaces--) cout << " ";
+        cout << "\\" << endl;
     }
+
+    cout << "/";
+    int underscores = 2*(M-1);
+    while(underscores--) cout <<"-";
+    cout << "\\" << endl;
 }
--- 3R/c2.cpp
+++ 3R/c2.cpp
@@ -1,23 +1,34 @@
-#include <iostream>
-#include <cstring>
+//題目的 M 是小 level，N是大 size
 
-using namespace std;
+#include <stdio.h>
 
-int main(){
-    string str;
-    while(getline(cin, str)){
-        int n = 0;
-        bool word = false;
-        for(int i=0; i<str.size(); i++){
-            if((str[i] >= 'a' && str[i] <='z') || str[i]>='A' && str[i]<='Z') word = true;
-            else if(word){
-                word = false;
-                n++;
+int main() {
+    int cases = 0, n, m;
+    while(scanf("%d %d", &n, &m) == 2) {
+        if(n == 0 && m == 0)
+            break;
+        int i, j, k, l;
+        int h = n*m-1; //前面要留多少空格
+        printf("Triangular Museum %d\n", ++cases);
+        for(i = 0; i < m; i++) {// 小 level
+            for(j = 0; j < n; j++) { //大 level
+                for(k = 0; k < h; k++)
+                    putchar(' ');
+                h--;
+                for(k = 0; k <= i; k++) {
+                    putchar('/');
+                    for(l = 0; l < 2*j; l++)
+                        putchar(j == n-1 ? '_': ' ');
+                    putchar('\\');
+                    if(k != i) {
+                        for(l = 2; l < 2*n-2*j; l++)
+                            putchar(' ');
+                    }
+                }
+                puts("");
             }
         }
-
-        if(word) n++;
-
-        cout << n << endl;
+        puts("");
     }
+    return 0;
 }
