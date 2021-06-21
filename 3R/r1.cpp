d0e0e56a52cc9fcd6ff18245f93c30c28a95e677
YenRotiCanai <lnight94@gmail.com>
2021-06-21 10:02:11 +0800

    8l

--- 3R/r1.cpp
+++ 3R/r1.cpp
@@ -1,55 +1,15 @@
 #include <iostream>
 #include <cmath>
-#include <map>
 #include <cstdio>
-
 using namespace std;
 
+//為了確保 double 的精準度，要加上一個 1e-9
+const double EPSILON = 1e-9;
+
 int main(){
-    unsigned long n;
-    map<int,int> mp;
-    map<int,int>::iterator iter;
+    int N;
 
-    while(scanf("%lu", &n) && n!=-1){
-        mp.clear();
-        
-        //找出因數，用 map 來統計出現過多少次
-        for(int i=2; i<sqrt(n); i++){
-            while(n%i == 0){
-                mp[i]++;
-                n /= i;
-            }
-        }
-        if(n>2) mp[n]++; //最後一個因數
-
-        //印出題目要的
-        for(iter = mp.begin(); iter!=mp.end(); iter++){
-            printf("(%d,%d)", iter->first, iter->second);
-        }
-        printf("\n");
-
-        int num = 1;
-        unsigned long sum = 1;
-        int pow_result;
-        int p;
-        
-        //根據題目的 formula 來算 no. of divisor 和 sum of divisor
-        for(iter = mp.begin(); iter!=mp.end(); iter++){
-            int a = iter->first;
-            int b = iter->second;
-
-            //算 no. of divisor
-            num *= (b+1);
-
-            //算 sum of divisor
-            p = 0;
-            for(int i=0; i<=b; i++){
-                pow_result = (int)(pow(a, i)+0.5);
-                p += pow_result;
-            }
-            sum *= p;
-        }
-        printf("%d\n%d\n===\n", num, sum);
+    while(scanf("%d", &N)!=EOF && N!=0){
+        int layer = (int)(sqrt((double)(N-1)) + EPSILON) + 1 ;
     }
-    return 0;
 }
