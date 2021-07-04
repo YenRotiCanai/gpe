35395ba5fa7e4ecf962b2da36c604342031dde60
YenRotiCanai <lnight94@gmail.com>
2021-07-05 07:43:42 +0800

    cd-904

--- 0714/c1.cpp
+++ 0714/c1.cpp
@@ -1,47 +1,36 @@
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#define N 100001
+#include <iostream>
+
 using namespace std;
 
 int main(){
-    char str[N], rev[N]; //用來存測資和 reverse 測資的陣列
+    long n, m;
+    int max_count=0;
 
-    while(scanf("%s", str)){
-        int fail[N] = { -1 }; //把第一個設為 -1
-        int len = strlen(str);
-        int i, j;
-        reverse_copy(str, str+len, rev); //反轉字串並存到 rev 裡面
+    while(cin >> n >> m){
+        cout << n << " " << m;
 
-        //failure function
-        for(i=1, j=-1; i<len; i++){
-            //printf("before: i:%d j:%d rev[i]:%c rev[j+1]:%c\n", i, j, rev[i], rev[j+1]);
-            
-            //如果 j 走到一半了（代表前面有相同的），可是相鄰的有不一樣，就把 j 改成 fail[j] 的值
-            while(j >= 0 && rev[j+1] != rev[i]) j = fail[j];
-
-            //如果前後都是一樣，就將 j 往後移，j++
-            if(rev[j+1] == rev[i]) j++;
-
-            //TO-DO 還要想想
-            //最後的 j 就是 fail 值，應該？
-            fail[i] = j;
-
-            //printf("after: j:%d fail[i]:%d\n\n", j, fail[i]);
+        if(n > m){
+            long tmp = n;
+            n = m;
+            m = tmp;
         }
 
-        //匹配，和 failure func 差不多
-        for(i = 0, j = -1; i < len; i++){
-            while(j>=0 && rev[j+1] != str[i]) j = fail[j];
+        int len = 1;
 
-            if(rev[j+1] == str[i]) j++;
+        for(int i=n; i<=m; i++){
+            long q = i;
+            len = 1;
+
+            while(true){
+                if(q == 1) break;
+                if(q%2 == 0) q/=2;
+                else q = 3*q+1;
+
+                len++;
+            }
+            max_count = max(max_count, len);
         }
 
-        for(i = 0; i<len; i++) printf("%c", str[i]);
-
-        for(j++; j<len; j++) printf("%c", rev[j]);
-
-        printf("\n");
+        cout << " " << max_count << endl;
     }
-    return 0;
 }
--- 0714/j1.java
+++ 0714/j1.java
@@ -1,7 +1,33 @@
 import java.util.*;
 
 class Main{
-    public static void main(String[] Args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNext()){
+        	long a = sc.nextLong();
+        	long b = sc.nextLong();
+
+        	System.out.print(a+" "+b+" ");
+
+        	if(a>b){
+        		long c = a; a = b; b = c;
+        	}
+
+        	long maxLen = 0;
+        	for(long i=a; i<=b; i++){
+        		long n = i, len = 1;
+
+        		while(true){
+        			if(n==1) break;
+        			if(n%2==1) n=3*n+1;
+        			else n/=2;
+
+        			len++;
+        		}
+        		maxLen = Math.max(maxLen, len);
+        	}
+        	System.out.println(maxLen);
+        }
     }
 }
