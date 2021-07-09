ec9ad6bed98c5de52d33644a8a392e6ecf83c673
YenRotiCanai <lnight94@gmail.com>
2021-07-09 18:54:22 +0800

    cd4

---
+++ 0714/j3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[][] = {
+            {1,2,3}, 
+            {4,5,6}
+        };
+
+        int len = arr.length;
+        int len2 = arr[0].length;
+
+        System.out.println(len);
+        System.out.println(len2);
+    }
+}
